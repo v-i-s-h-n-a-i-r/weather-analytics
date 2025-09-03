79ebeef2df8d3ab2962ee2e4db61509dd35ea39c
v-i-s-h-n-a-i-r <vnair.reality@gmail.com>
2025-09-03 21:31:19 +0530

    Add simple /notifications route with mock notification message

--- src/app.py
+++ src/app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Hello, World! from Flask"
+
+
+# Simple notifications route
+@app.route('/notifications')
+def notifications():
+    mock_notification = {
+        "message": "You have a new weather alert!",
+        "type": "alert"
+    }
+    return jsonify(mock_notification)
 
 if __name__ == "__main__":
     app.run(debug=True)
